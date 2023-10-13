71cc7ecf57fc19a067121117c3c601deb935d8d2
Evansrintari <rintari110@gmail.com>
2023-10-13 09:34:21 +0300

    empmanagmentsys first commit

---
+++ src/com/sj/empmgmtapplication/EmployeeService.java
@@ -0,0 +1,4 @@
+package com.sj.empmgmtapplication;
+
+public class EmployeeService {
+}
---
+++ src/com/sj/empmgmtapplication/Main.java
@@ -0,0 +1,20 @@
+package com.sj.empmgmtapplication;
+import java.util.Scanner;
+
+public class Main {
+    EmployeeService servie =new EmployeeService();
+    static boolean ordering =true;
+    public static void menu(){
+        System.out.println("*********Welcome to the employee service managment service***** "
+                + "\n1. Add Employee "
+                +"\n2. Veiw Employee "
+                +"\n3. Update employee "
+                +"\n4. Delete Employee "
+                +"\n5. Veiw All Employee "
+                +"\n6. Exits");
+
+    }
+    public static void main(String[] args) {
+
+        }
+    }
